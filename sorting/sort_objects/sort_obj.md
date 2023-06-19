6bdbb1fc34005f10e46ce38986b4ed87e14a9d2f
Sara-Alkhateeb <sarajamal11399@yahoo.com>
2023-06-20 02:51:55 +0300

    cc28 done

---
+++ sorting/sort_objects/sort_obj.md
@@ -0,0 +1,6 @@
+# Sort Object
+     Sort Object sort is a simple sorting algorithm that builds the final sorted  list by title or recent year.
+
+## Link of the code
+### [click here to the Sort Object code](./sort_objects.py)
+### [click here to the Sort Object test](../../tests/test_sort_obj.py)
--- sorting/sort_objects/sort_objects.py
+++ sorting/sort_objects/sort_objects.py
@@ -1,66 +1,65 @@
-movies = [
-    {
-        "title": "The Shawshank Redemption",
-        "year": 1994,
-        "genres": ["Drama", "Crime"]
-    },
-    {
-        "title": "The Godfather: Part II",
-        "year": 1974,
-        "genres": ["Crime", "Drama"]
-    },
-    {
-        "title": "The Dark Knight",
-        "year": 2008,
-        "genres": ["Action", "Crime", "Drama", "Thriller"]
-    },
-    {
-        "title": "pulp fiction",
-        "year": 1994,
-        "genres": ["Crime", "Drama"]
-    },
-    {
-        "title":"Inception",
-        "year": 2010,
-        "genres": ["Action", "Crime", "Drama", "Mystery", "Sci-Fi", "Thriller"]
-    },
-    {
-        "title":"Interstellar",
-        "year": 2014,
-        "genres": ["Adventure", "Drama", "Sci-Fi"]
-    },
-    {
-        "title": "Fight Club",
-        "year": 1999,
-        "genres": ["Drama"]
-    },
-]
+import re
+array = [
+        {
+            "title": "The Shawshank Redemption",
+            "year": 1994,
+            "genres": ["Drama", "Crime"]
+        },
+        {
+            "title": "The Godfather",
+            "year": 1972,
+            "genres": ["Crime", "Drama"]
+        },
+        {
+            "title": "The Dark Knight",
+            "year": 2008,
+            "genres": ["Action", "Crime", "Drama", "Thriller"]
+        },
+        {
+            "title": "Pulp Fiction",
+            "year": 1994,
+            "genres": ["Crime", "Drama"]
+        },
+        {
+            "title": "Inception",
+            "year": 2010,
+            "genres": ["Action", "Crime", "Drama", "Mystery", "Sci-Fi", "Thriller"]
+        },
+        {
+            "title": "Schindler's List",
+            "year": 1993,
+            "genres": ["Biography", "Drama", "History"]
+        },
+        {
+            "title": "Interstellar",
+            "year": 2014,
+            "genres": ["Adventure", "Drama", "Sci-Fi"]
+        },
+    ]
 
-def sort_movies_by_year(movies):
-    sorted_movies = sorted(movies, key=lambda movie: movie['year'], reverse=True)
-    return sorted_movies
+def sort_by_most_recent(array):
+    sorted_list = sorted(array, key=lambda x: x['year'], reverse=True)
+    return sorted_list
 
-def sort_movies_by_title(movies):
-    def remove_leading_articles(title):
-        articles = ['The ', 'An ', 'A ']
-        for article in articles:
-            if title.startswith(article):
-                return title[len(article):]
-        return title
+def sort_alphabetical(array):
+    sorted_list = sorted(array, key=lambda x: re.sub(r'^(A *|An *|The *)\s', '', x['title'], flags=re.IGNORECASE))
+    return sorted_list
 
-    sorted_movies = sorted(movies, key=lambda movie: remove_leading_articles(movie['title'].lower()))
-    return sorted_movies
 
 if __name__ == '__main__':
-    sorted_by_year = sort_movies_by_year(movies)
-    print('Sorted by year:')
-    for movie in sorted_by_year:
-        print(movie['title'], movie['year'])
+        sorted_by_year = sort_by_most_recent(array)
+        print('Sorted by year:')
+        for movie in sorted_by_year:
+            print(movie['title'], movie['year'])
 
-    sorted_by_title = sort_movies_by_title(movies)
-    print('\nSorted by title:')
-    for movie in sorted_by_title:
-        print(movie['title'], movie['year'])
+        sorted_by_title = sort_alphabetical(array)
+        print('\nSorted by title:')
+        for movie in sorted_by_title:
+            print(movie['title'], movie['year'])
+
+        print(sort_alphabetical(array))
+
+
 
 
     
